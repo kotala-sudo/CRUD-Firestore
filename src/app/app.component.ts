f8ac431c520a3d5a4a61d4e0a6d192b2a2186e5b
kotala-sudo <pkotala@yahoo.com>
2025-04-10 14:52:47 -0500

    added bootstrap and list of names with alice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,8 @@ export class AppComponent {
     email: '',
   }
 
+  usersWithNameAlice: User[] = [];
+
   editingUserId: string | null = null; // Variable to store the ID of the user being edited
 
   userService = inject(UserService); // Inject the UserService
@@ -28,6 +30,10 @@ export class AppComponent {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
     });
+
+    this.userService.getUserByName('Alice').subscribe(users =>
+      this.usersWithNameAlice = users
+    )
   }
 
   addUser() {
@@ -37,22 +43,17 @@ export class AppComponent {
     }
     this.user = { id: '', name: '', email: '' }; // Reset the user object after adding
   }
+
   editUser(user: User) {
     this.editingUserId = user.id ?? null; // Set the ID of the user being edited
     this.user = { ...user }; // Populate the form with the user's data
   }
+
   updateUser() {    
-    // if (this.user.name && this.user.email) {
-    //   this.userService.updateUser(this.user.id, this.user).then(() => {
-    //     this.user = { id: '', name: '', email: '' }; // Reset the user object
-    //     this.editingUserId = null; // Clear the editing user ID
-    //   });
-    // }
         if (this.user.name && this.user.email)
             this.userService.updateUser(this.user.id, this.user)
         this.user = { id: '', name: '', email: '' }; // Reset the user object
         this.editingUserId = null; // Clear the editing user ID
-      
     }
 
   deleteUser(id: string) {  
