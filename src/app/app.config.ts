5dcebaf7b0f4f3a0a76defa047a1c3e345b6eaeb
kotala-sudo <pkotala@yahoo.com>
2025-04-14 09:48:32 -0500

    Removed the firebaseConfig object from app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,14 @@ import { routes } from './app.routes';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
+//replace the firebaseConfig object with one from your firebase project
 const firebaseConfig = {
-  apiKey: "AIzaSyBRaNQ4DlRayamHnL0Ln-yUY5t7fSnuRA4",
-  authDomain: "crud-firestore-project.firebaseapp.com",
-  projectId: "crud-firestore-project",
-  storageBucket: "crud-firestore-project.firebasestorage.app",
-  messagingSenderId: "750156576536",
-  appId: "1:750156576536:web:efe0ca0943d5c9aefe106d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const appConfig: ApplicationConfig = {
@@ -20,6 +21,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
-    { provide: FIREBASE_OPTIONS, useValue: firebaseConfig }  
   ]
 }; 
